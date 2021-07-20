3fcef383af2de34ab7999bd6f4dc7aa2aa01bb9c
Prasadchaskar <chaskarprasad2000.com>
2021-07-20 15:37:52 +0530

    Updated

--- shopMart/admin.py
+++ shopMart/admin.py
@@ -3,8 +3,5 @@ from . models import *
 # Register your models here.
 
 admin.site.register(Categrory)
-admin.site.register(thredType)
 admin.site.register(Color)
-admin.site.register(Material)
-admin.site.register(Shape)
 admin.site.register(Product)
--- shopMart/models.py
+++ shopMart/models.py
@@ -1,51 +1,34 @@
 from django.db import models
-
+import datetime
 # Create your models here.
 class Categrory(models.Model):
-    category_Name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
-        return self.category_Name
+        return self.name
 
-class thredType(models.Model):
-    thred_type = models.CharField(max_length=50)
-
-    def __str__(self) -> str:
-        return self.thred_type
 
 class Color(models.Model):
-    color_Name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
-        return  self.color_Name
-class Material(models.Model):
-    material_Name = models.CharField(max_length=50)
-
-    def __str__(self) -> str:
-        return self.material_Name
-
-class Shape(models.Model):
-    shape_Name = models.CharField(max_length=50)
-
-    def __str__(self) -> str:
-        return self.shape_Name
+        return  self.name
 
 class Product(models.Model):
     product_Name        = models.CharField(max_length=100)
     price               = models.IntegerField()
     category            = models.ForeignKey(Categrory,on_delete=models.CASCADE)
-    material            = models.ForeignKey(Material,on_delete=models.CASCADE)
-    shape               = models.ForeignKey(Shape,on_delete=models.CASCADE)
     color               = models.ForeignKey(Color,on_delete=models.CASCADE)
-    weight              = models.CharField(max_length=50)
-    dimensions          = models.CharField(max_length=50)
-    thread_Type         = models.ForeignKey(thredType,on_delete=models.CASCADE)
-    age_Group           = models.CharField(max_length=100)
+    short_decription    = models.CharField(max_length=600)
     decription          = models.TextField()
     main_Img            = models.ImageField(upload_to="myApp/images",default="")
     img1                = models.ImageField(upload_to="myApp/images",default="")
     img2                = models.ImageField(upload_to="myApp/images",default="")
     img3                = models.ImageField(upload_to="myApp/images",default="")
     img4                = models.ImageField(upload_to="myApp/images",default="")
+    is_available        = models.BooleanField(default=True)
+    is_active           = models.BooleanField(default=True)
+    created             = models.DateField(auto_now_add=True)
+
     def __str__(self) -> str:
         return self.product_Name
