80ff8cd59a9313c52385945ad5374245ccfb6c21
Prasadchaskar <chaskarprasad2000.com>
2021-08-14 20:36:27 +0530

    models.py Updated

--- shopMart/models.py
+++ shopMart/models.py
@@ -15,23 +15,28 @@ class Color(models.Model):
         return  self.name
 
 class Product(models.Model):
-    product_Name            =   models.CharField(max_length=100)
-    price                   =   models.IntegerField()
-    category                =   models.ForeignKey(Categrory,on_delete=models.CASCADE)
-    color                   =   models.ForeignKey(Color,on_delete=models.CASCADE)
-    short_decription        =   models.CharField(max_length=600)
-    decription              =   models.TextField()
-    main_Image              =   models.FileField(blank=True)
-    is_available            =   models.BooleanField(default=True)
-    is_active               =   models.BooleanField(default=True)
-    created                 =   models.DateField(auto_now_add=True)
+    title                   =       models.CharField(max_length=100)
+    price                   =       models.IntegerField()
+    category                =       models.ForeignKey(Categrory,on_delete=models.CASCADE)
+    color                   =       models.ForeignKey(Color,on_delete=models.CASCADE)
+    short_decription        =       models.CharField(max_length=600)
+    decription              =       models.TextField()
+    main_image              =       models.FileField(blank=True)
+    is_available            =       models.BooleanField(default=True)
+    is_active               =       models.BooleanField(default=True)
+    created                 =       models.DateField(auto_now_add=True)
 
     def __str__(self) -> str:
-        return self.product_Name
+        return self.title
+    
+    def get_images(self):
+        return ProductImage.objects.filter(product=self)
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
-    images = models.FileField(upload_to="myApp/images")
- 
+    image = models.ImageField(upload_to="myApp/images")
+
     def __str__(self):
-        return self.product_Name
+        return str(self.product)
+ 
+   
--- shopMart/views.py
+++ shopMart/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import Product
+from . models import Product,ProductImage
 # Create your views here.
 def home(request):
     products = Product.objects.all()
@@ -9,5 +9,7 @@ def home(request):
 def Details(request,myid):
     all_products = Product.objects.all()
     products=Product.objects.filter(id=myid)
+    object = Product.objects.get(pk=myid)
+    object.get_images()
     return render(request,'productDetails.html',{'products':products[0],'all_products':all_products})
  
